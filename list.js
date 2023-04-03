a7a004555d960769bc04de53f81202b1f153b0fd
Jie C <117114870+jie5566@users.noreply.github.com>
2023-04-03 23:14:14 +0200

    set the condition for addtodo
    now it is only work when people type in something

--- list.js
+++ list.js
@@ -10,39 +10,42 @@ document.addEventListener("DOMContentLoaded", loadToDo);
 //The DOMContentLoaded event fires when the HTML document has been completely parsed, and all deferred scripts (<script defer src="…"> and <script type="module">) have downloaded and executed. It doesn't wait for other things like images, subframes, and async scripts to finish loading.
 
 function addToDo() {
-  const toDoParagraph = document.createElement("p");
-  toDoParagraph.innerText = inputToDo.value;
-  toDoParagraph.classList.add("todo-item");
-  todoContainer.appendChild(toDoParagraph);
+  if (inputToDo.value.length > 0) {
+    const toDoParagraph = document.createElement("p");
+    toDoParagraph.innerText = inputToDo.value;
+    toDoParagraph.classList.add("todo-item");
+    todoContainer.appendChild(toDoParagraph);
 
-  const finishBTN = document.createElement("button");
-  finishBTN.innerText = "✓";
-  finishBTN.classList.add("finish-button");
-  todoContainer.appendChild(finishBTN);
+    const finishBTN = document.createElement("button");
+    finishBTN.innerText = "✓";
+    finishBTN.classList.add("finish-button");
+    todoContainer.appendChild(finishBTN);
 
-  const removeBTN = document.createElement("button");
-  removeBTN.innerText = "−";
-  removeBTN.classList.add("remove-button");
-  todoContainer.appendChild(removeBTN);
+    const removeBTN = document.createElement("button");
+    removeBTN.innerText = "−";
+    removeBTN.classList.add("remove-button");
+    todoContainer.appendChild(removeBTN);
 
-  inputToDo.value = "";
+    inputToDo.value = "";
 
-  finishBTN.addEventListener("click", function (event) {
-    //this are two ways to target the element I create in JS, not in the HTML
-    // console.log(event.currentTarget);
+    finishBTN.addEventListener("click", function (event) {
+      //this are two ways to target the element I create in JS, not in the HTML
+      // console.log(event.currentTarget);
 
-    // finishBTN.addEventListener("click", (event) => {
-    //   console.log("Start the game!");
-    // });
+      // finishBTN.addEventListener("click", (event) => {
+      //   console.log("Start the game!");
+      // });
 
-    toDoParagraph.style.textDecoration = "line-through";
-  });
+      toDoParagraph.style.textDecoration = "line-through";
+    });
 
-  removeBTN.addEventListener("click", function () {
-    todoContainer.removeChild(toDoParagraph);
-    todoContainer.removeChild(finishBTN);
-    todoContainer.removeChild(removeBTN);
-  });
+    removeBTN.addEventListener("click", function () {
+      todoContainer.removeChild(toDoParagraph);
+      todoContainer.removeChild(finishBTN);
+      todoContainer.removeChild(removeBTN);
+    });
+  }
+
   saveToDo();
   // localStorage.setItem("todoList", todoContainer.innerHTML);
   //the method has some issues because the localstorage cannot store the whole function of addtodo, too much information.
